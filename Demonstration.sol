bfce7060d682d6d7d295c5c63ddc04d6044b31d8
Valerie Andy <valdaraa@protonmail.com>
2022-11-14 05:50:01 -0500

    Update Demonstration.sol

--- Demonstration.sol
+++ Demonstration.sol
@@ -1,1 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.16;
 
+contract Demonstration {
+    uint public count;
+
+    function increment() external {
+        count += 1;
+    }
+}
+
+interface IDemonstration {
+    function count() external view returns (uint);
+
+    function increment() external;
+}
+
+contract MyContract {
+    function incrementCounter(address _counter) external {
+        IDemonstration(_counter).increment();
+    }
+
+    function getCount(address _counter) external view returns (uint) {
+        return IDemonstration(_counter).count();
+    }
+}
